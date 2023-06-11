120ea214410de75f3ac35424bb8ee56bdc9a061d
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-06-11 11:18:02 -0400

    Handling Requests and Redirecting

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,14 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET','POST'])
+def index():
+    print(request.form)
+    redirect('/facts')
+
+    return  render_template('facts/index.html')
+    
 
 @bp.route('/new')
 def new(): 
